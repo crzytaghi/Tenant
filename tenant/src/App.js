51cde904bb0004790fab7c4ce7aeee6789dc63b5
Andrew Taghi <aataghi13@gmail.com>
2020-08-13 14:22:13 -0500

    adding contents

--- tenant/src/App.js
+++ tenant/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    username:'Kyle'
+  }
+
   render = () => {
     return (
-    <div>
-      <h1>Tenant</h1>
+    <div className="container">
+      <div className="sidebar">
+        <h1>Tenant</h1>
+        <h2>Welcome {this.state.username}</h2>
+      </div>
+      <div className="main">
+        
+      </div>
     </div>
     )
   }
